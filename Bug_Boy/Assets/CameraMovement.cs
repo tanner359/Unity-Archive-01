25e19d8c39bcf200755b10f8b15def7244f0b6c6
Tanner Cobb <tannercobb359@gmail.com>
2022-12-30 02:38:44 -0500

    Bug Boy - Archive

---
+++ Bug_Boy/Assets/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float camera_Speed;
+    public float zOffset;
+    public float yOffset;
+    public float xOffset;
+    public GameObject player;
+
+
+    private void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + new Vector3(xOffset, yOffset, zOffset), camera_Speed);
+        if (player.GetComponent<Rigidbody>().velocity.y <= -3.5f)
+        {
+            if (camera_Speed < 1f)
+            {
+                camera_Speed += 0.4f * Time.deltaTime;
+            }
+        }
+        else
+        {
+            camera_Speed = 0.08f;
+        }
+        if (player.GetComponent<Controller>().flying == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position + new Vector3(xOffset, yOffset + 3, zOffset), camera_Speed - 0.02f);
+        }        
+    }
+   
+}
---
+++ Bug_Boy/Assets/Scripts/Controller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Animator animator;
+
+    public float movementSpeed;
+    public float rotateSpeed;
+
+    public bool flying = true;
+    private bool stickbug = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input_Vec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (input_Vec.y > 0)
+        {
+            animator.SetBool("Walking", true);
+            gameObject.transform.position += -transform.right * movementSpeed * Time.deltaTime;
+        }
+        else if(input_Vec.y < 0)
+        { 
+            gameObject.transform.position += transform.right * movementSpeed * Time.deltaTime;
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+        if(input_Vec.x != 0)
+        {
+            transform.Rotate(0, input_Vec.x * rotateSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Flying", flying);
+            flying = !flying;
+            if (!flying)
+            {
+
+            }
+        }       
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.SetBool("Stickbugged", stickbug);
+            stickbug = !stickbug;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            animator.SetTrigger("Tail");
+        }
+    }
+}
